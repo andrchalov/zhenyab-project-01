64f49823fa7d69bd0019125f5e11970eb0dcf447
Andrey Chalov <andrchalov@gmail.com>
2018-11-13 00:27:16 +0300

    add proxy to https api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,13 +6,12 @@
           <b-card-group>
             <b-card no-body class="p-4">
               <b-card-body>
-                <b-form>
+                <b-form @submit="submit">
                   <h1>Login</h1>
                   <p class="text-muted">Sign In to your account</p>
-                  <p>Your token: {{accessToken}}</p>
                   <b-input-group class="mb-3">
                     <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input v-model="username" type="text" class="form-control" placeholder="Username" autocomplete="username email" />
+                    <b-form-input v-model="username" type="text" class="form-control" placeholder="Username" autocomplete="username email"/>
                   </b-input-group>
                   <b-input-group class="mb-4">
                     <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
@@ -20,7 +19,7 @@
                   </b-input-group>
                   <b-row>
                     <b-col cols="4">
-                      <b-button @click="submit" variant="primary" class="px-4">Login</b-button>
+                      <b-button type="submit" variant="primary" class="px-4">Login</b-button>
                     </b-col>
                     <b-col cols="8">
                       <span class="text-danger" v-if="error">{{error}}</span>
